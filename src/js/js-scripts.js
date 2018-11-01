fa4e03b3cf0a7bc9d3119278cfd10f5ce211f1fe
Polak, Daniel <daniel.polak@sap.com>
2018-11-01 13:54:06 +0100

    unnecesary code cleanup

--- src/js/js-scripts.js
+++ src/js/js-scripts.js
@@ -1,25 +1,16 @@
 
 
-(function ($, window, document, undefined) {
-
-  'use strict';
-
-  $(function () {
-    // FastShell
-  });
-
-})(jQuery, window, document);
-
 /* Custom js - theme related */
 
-
 /* Custom js - theme related - end */
+
 
 
 
 /*  */
 /* Wordpress related - underscores theme */
 /*  */
+
 
 /**
  * File skip-link-focus-fix.js.
@@ -163,45 +154,3 @@
 	}( container ) );
 } )();
 
-/**
- * File customizer.js.
- *
- * Theme Customizer enhancements for a better user experience.
- *
- * Contains handlers to make Theme Customizer preview reload changes asynchronously.
- */
-if ( typeof wp.customize != "undefined" ) {
-	( function( $ ) {
-			// Site title and description.
-			wp.customize( 'blogname', function( value ) {
-				value.bind( function( to ) {
-					$( '.site-title a' ).text( to );
-				} );
-			} );
-			wp.customize( 'blogdescription', function( value ) {
-				value.bind( function( to ) {
-					$( '.site-description' ).text( to );
-				} );
-			} );
-
-			// Header text color.
-			wp.customize( 'header_textcolor', function( value ) {
-				value.bind( function( to ) {
-					if ( 'blank' === to ) {
-						$( '.site-title, .site-description' ).css( {
-							'clip': 'rect(1px, 1px, 1px, 1px)',
-							'position': 'absolute'
-						} );
-					} else {
-						$( '.site-title, .site-description' ).css( {
-							'clip': 'auto',
-							'position': 'relative'
-						} );
-						$( '.site-title a, .site-description' ).css( {
-							'color': to
-						} );
-					}
-				} );
-			} );
-	} )( jQuery );
-}
